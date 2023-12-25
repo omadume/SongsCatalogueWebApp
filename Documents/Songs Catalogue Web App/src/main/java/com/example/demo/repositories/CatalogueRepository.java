ad54a0c444d668cf6ce58eee6bf0dbf0086e2084
Olivia Madume <oliviamadume@Olivias-MBP.Home>
2023-12-25 14:43:26 +0000

    removing old base

--- Documents/Songs Catalogue Web App/src/main/java/com/example/demo/repositories/CatalogueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Catalogue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CatalogueRepository extends JpaRepository<Catalogue, Long> {
-}
--- Documents/Songs Catalogue Web App/src/main/java/com/example/demo/repositories/SongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-
-    boolean existsBySongNameAndArtistNameAndCatalogueId(String songName, String artistName, Long id);
-}
